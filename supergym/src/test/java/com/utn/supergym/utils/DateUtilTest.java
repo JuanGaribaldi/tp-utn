cd3a30f3dfa7f8328a7c668d991f77625bf35adc
Juan_Garibaldi <jgaribaldi.itr@supervielle.com.ar>
2024-11-04 15:19:12 -0300

    WIP - Agregado de test DateUtilTest

--- supergym/src/test/java/com/utn/supergym/utils/DateUtilTest.java
+++ supergym/src/test/java/com/utn/supergym/utils/DateUtilTest.java
@@ -1,21 +1,57 @@
 package com.utn.supergym.utils;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@SpringBootTest
 class DateUtilTest {
 
+    private final LocalDate localDate = LocalDate.of(2024, 01, 02);
+    private final LocalTime localTime = LocalTime.of(12, 11, 12);
+    private final LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
+
+    private final String FECHA_STRING = "2024-01-02 12:11:12";
+    private final String EXPECTED_DATE_TIME_STRING = localDate.toString() + " " + localTime.toString();
+    private final String EXPECTED_DATE_STRING = localDate.toString();
+
     @Test
-    void parseLocalDateTime() {
-        //TODO
+    void parseLocalDateTimeConDatoNoNull() {
+        LocalDateTime parsedLocalDateTime = DateUtil.parseLocalDateTime(FECHA_STRING);
+
+        Assertions.assertEquals(localDateTime, parsedLocalDateTime);
     }
 
     @Test
-    void toDateTimeString() {
-        //TODO
+    void parseLocalDateTimeConDatoNull() {
+        Assertions.assertNull(DateUtil.parseLocalDateTime(null));
     }
 
     @Test
-    void toDateString() {
-        //TODO
+    void toDateTimeStringConDatoNoNull() {
+        String dateTimeString = DateUtil.toDateTimeString(localDateTime);
+
+        Assertions.assertEquals(EXPECTED_DATE_TIME_STRING, dateTimeString);
+    }
+
+    @Test
+    void toDateTimeStringConDatoNull() {
+        Assertions.assertNull(DateUtil.toDateTimeString(null));
+    }
+
+    @Test
+    void toDateStringConDatoNoNull() {
+        String dateString = DateUtil.toDateString(localDateTime);
+
+        Assertions.assertEquals(EXPECTED_DATE_STRING, dateString);
+    }
+
+    @Test
+    void toDateStringConDatoNull() {
+        Assertions.assertNull(DateUtil.toDateString(null));
     }
 }
